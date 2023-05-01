3592ab1f2f770d531f80bdb1d8475457f82fdf19
Milton Chandradas <milton.chandradas@live.com>
2023-04-30 22:37:43 -0400

    data binding...

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,47 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller",
+    "com/sap/lmui5tutorial/utils/filterUtils",
+    "sap/ui/model/json/JSONModel",],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
-        "use strict";
-
-        return Controller.extend("com.sap.lmui5tutorial.controller.Main", {
-            onInit: function () {
-
-            }
-        });
-    });
+    function (Controller, filterUtils, JSONModel) {
+      "use strict";
+  
+      return Controller.extend("com.sap.lmui5tutorial.controller.Main", {
+        onInit: function () {
+  
+          // Setting the view model for busy indicators
+          let viewModel = new JSONModel({
+              busy: false,
+              delay: 0,
+              currency: "USD",
+            });
+  
+    
+            this.getView().setModel(viewModel, "viewModel");
+            this._viewModel = this.getView().getModel("viewModel");
+            
+            this._mainModel = this.getOwnerComponent().getModel();
+            this._filterArray = filterUtils.getFilterArray(this);
+  
+        },
+  
+        /***********************************************************************************************/
+        /*		FILTER BAR EVENT HANDLERS
+        /***********************************************************************************************/
+  
+        onFilterBarChange: function () {
+          this._filterArray = filterUtils.getFilterArray(this);
+        },
+  
+        onBeforeRebindTable: function (oEvent) {
+          let bindingParams = oEvent.getParameter("bindingParams");
+          bindingParams.filters = filterUtils.getFilterArray(this);
+  
+          this._mainModel.refresh();
+        },
+      });
+    }
+  );
+  
